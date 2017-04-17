4831a15013a31ce6bb67aa4f648280bfb1435786
Megadojd <megadojd@mail.ru>
2017-04-17 18:34:34 +0300

    17.04.2017

--- Graf.js
+++ Graf.js
@@ -1,7 +1,7 @@
 class Node {
 constructor (name) {
         this.name = name;
-		this.value;
+		//this.value;
 		this.visited=false;
         this.children = [];
 	}
@@ -14,7 +14,7 @@ class Graf {
 		let createdNodes = new Map();
         for (let edge of edges) {
             let node1, node2;
-			let [name1, name2] = edge;
+			let [name1, name2, weight] = edge;
             
 			if (!createdNodes.has(name1)){
 				node1=new Node(name1);
@@ -33,7 +33,7 @@ class Graf {
             else {
 				node2 = createdNodes.get(name2);
 			}
-			node1.children.push(node2);
+			node1.children.push([node2, weight]);
 		}	
 	}
 
@@ -41,7 +41,7 @@ class Graf {
         if (root == null) return;
 		console.log(root.name);
 		root.visited = true;
-		for(let child of root.children){
+		for(let [child, weight] of root.children){
 			if(!child.visited) {
 				this.dfs(child);
 			}
@@ -57,7 +57,7 @@ bfs (root) {
 	while (queue.length>0) {
 		let current=queue.shift();
 		console.log(current.name);
-		for (let node of current.children) {
+		for (let [node, weight] of current.children) {
 			if (!node.visited) {
 				node.visited=true;
 				queue.push(node);
@@ -68,7 +68,39 @@ bfs (root) {
 }
 
 
-bfs_min (root) {
+bfs_min (start, end) {
+	if (!start) {
+		return undefined;
+	}
+	if (!start) {
+		return undefined;
+	}
+	let path=[];
+
+
+	let queue=[];
+	queue.push([start, 0]);
+	while (queue.length>0) {
+		let [current, weight]=queue.shift();
+		console.log(current.name);
+
+
+		let children=Object.assign([], current.children);
+		children.sort(sortFunction);
+
+		for (let [node, weight] of children) {
+			if (!node.visited) {
+				node.visited=true;
+				queue.push([node, weight]);
+			}
+		}
+	} 
+
+}
+
+
+
+bfs_min2 (root) {
 	if (!root) {
 		return undefined;
 	}
@@ -98,5 +130,18 @@ if (node.value<min) {
 }
 
 
-let graf = new Graf ([[0,1],[0,4],[0,5],[1,3],[1,4],[3,4],[3,2],[2,1]]);
-graf.bfs(graf.nodes[0]);
+//let graf = new Graf ([[0,1],[0,4],[0,5],[1,3],[1,4],[3,4],[3,2],[2,1]]);
+
+let graf = new Graf ([[0,1,7],[0,2,1],[0,3,4],[1,4,2],[2,5,3],[3,7,3],[4,6,2],[5,4,1],[5,8,1],[8,7,1]]);
+
+//graf.bfs(graf.nodes[0]);
+graf.bfs_min(graf.nodes[0]);
+
+	function sortFunction(a, b){
+	  if(a[0] < b[0])
+	     return -1 // Или любое число, меньшее нуля
+	  if(a[0] > b[0] )
+	     return 1  // Или любое число, большее нуля
+	  // в случае а = b вернуть 0
+	  return 0
+	}
